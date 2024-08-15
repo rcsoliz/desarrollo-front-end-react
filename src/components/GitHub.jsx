e1bdf930a291d36c5ce6c3800374298e4e5a545f
rcsoliz <robertoucbnh@gmail.com>
2024-08-14 21:01:24 -0400

    241408-01

---
+++ src/components/GitHub.jsx
@@ -0,0 +1,16 @@
+const GitHub = () => {
+    return (
+        <>
+            <a
+              className="App-link"
+              href="https://github.com/rcsoliz/desarrollo-front-end-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+                Mi proyecto React (Git)
+            </a>
+        </>
+    )
+}
+
+export default GitHub
